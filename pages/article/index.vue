744283ba5daed4b95c04d9e73caeaa54a63991ac
rezza <rezza.fadillah@ai.astra.co.id>
2022-07-04 19:58:25 +0800

    fix articles page

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -1,75 +1,105 @@
 <template>
-  <div>
-    <Navbar />
-    <div class="container max-w-6xl my-12 mx-auto px-4 md:px-12 min-h-screen">
-      <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <div class="container my-12 mx-auto px-4 md:px-12">
-          <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <!-- Column -->
-            <div
-              class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
-              v-for="datapost in post"
-              :key="datapost.id"
-            >
-              <!-- Article -->
-              <article class="overflow-hidden border h-96">
-                <div class="h-56 w-full bg-blue-200">
-                  <nuxt-link :to="/Article/ + datapost.id"  href="#">
-                    <!-- <img :src="!datapost.image ? 'https://picsum.photos/600/400/?random' : datapost.image  "> <img/> -->
-                    <img :src="datapost.image" alt="image not available" class="h-full"/>
-
-                  </nuxt-link>
-                </div>
-                <header
-                  class="flex items-center justify-between leading-tight px-2 md:px-4"
-                >
-                  <h1 class="text-lg text-bold">
-                    {{ datapost.title }}
-                  </h1>
-                </header>
-                <p class="px-2 md:px-4 text-gray-400">{{ datapost.short_description }}</p>
-              </article>
-              <!-- END Article -->
+  <div class="bg">
+    <LoadSpinner v-if="showHideSpinner" />
+    <b-container style="margin-top: 5vh">
+      <b-row class="mt-5 mb-5">
+        <b-col
+          v-for="dataArticles in dataArticle"
+          :key="dataArticles.id"
+          lg="4"
+          md="6"
+          sm="12"
+          class="text-center mb-4"
+        >
+          <nuxt-link style="color: black" :to="'/article/' + dataArticles.id">
+            <b-img :src="dataArticles.image" alt="Left image" fluid></b-img>
+            <div class="title text-left">
+              {{ dataArticles.title }}
             </div>
-            <!-- END Column -->
-          </div>
-        </div>
-      </div>
-    </div>
-    <Footer />
+            <div class="subtitle text-left">
+              {{ dataArticles.short_description }}
+            </div>
+          </nuxt-link>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
-  name: "IndexPage",
+  name: 'Article',
   data() {
     return {
-      post: [],
-    };
+      title: '',
+      shortdesc: '',
+      image: '',
+      dataArticle: [],
+      showHideSpinner: true,
+    }
   },
-
-  mounted() {
-    this.getAllDataArticle();
+  beforeCreate() {
+    this.showHideSpinner = true
   },
-
+  created() {
+    this.getArticle()
+  },
   methods: {
-    async getAllDataArticle() {
-      await axios
-        .get("https://article-backend-assessment.herokuapp.com/api/articles")
-        .then((Response) => {
-          if (Response.status === 200) {
-            this.post = Response.data.content;
-          }
+    async getArticle() {
+      await this.$axios
+        .get('/api/articles', {
+          headers: { Authorization: 'Bearer ' + this.$cookies.get('token') },
+        })
+        .then((response) => {
+          this.showHideSpinner = false
+          // console.log(response.data.content)
+          const dataFilter = response.data.content.filter(
+            (el) =>
+              el.image.includes('.png') ||
+              el.image.includes('.jpeg') ||
+              el.image.includes('.jpg')
+          )
+          this.dataArticle = dataFilter
+          console.log('filter data', this.dataArticle)
         })
         .catch((err) => {
-          console.log(err);
-        });
+          this.showHideSpinner = false
+          if (!err) {
+          } else {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+            // this.resetLoginForm()
+            if (err.response.status === 409) {
+              this.$toast.error(err.response.data.message.details[0].message)
+            } else {
+              this.$toast.error(err.response.data.message)
+            }
+          }
+        })
     },
   },
-};
+}
 </script>
-
-<style></style>
+<style scoped>
+.bg {
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.title {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  margin-bottom: 8px;
+  margin-top: 16px;
+}
+.subtitle {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+}
+</style>
